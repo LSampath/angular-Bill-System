56e97540a6428903faffc7da3e912fa40dd25b2b
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2017-09-30 12:35:45 +0530

    Invoice interfaces done almost

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  username: string;
 
-  constructor() { }
+  constructor(private user: UserService) {
+    this.username = user.getUserName();
+  }
 
   ngOnInit() {
   }
